fa4fc895b613730836582d0323b62428f6ca8002
sashko-kapillar <kapillarua@gmail.com>
2024-10-12 09:21:41 +0300

    fix main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 import { form, emailInput, errorMessage } from './js/work-together';
-=======
-import { form } from './js/work-together';
+// import { form } from './js/work-together';
 import { themeToggle } from './js/toogle';
->>>>>>> main
