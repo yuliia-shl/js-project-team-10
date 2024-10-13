0db31c7c582d1a3b0304dc8410f404aa9595cc31
sashko-kapillar <kapillarua@gmail.com>
2024-10-13 19:08:08 +0300

    and to fix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-import { form, emailInput, errorMessage } from './js/work-together';
+import { form, emailInput, errorMessage } from './js/work-together.js';
 import { themeToggle } from './js/toogle.js';
-// import { form } from './js/work-together';
-// import { themeToggle } from './js/toogle';
-
 import { initHeader } from './js/menu.js';
+
 initHeader();
