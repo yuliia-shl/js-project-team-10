2cf9f902ca8ea343595f3e728103e076882ae4d0
Mishagul <169081080+Mishagul@users.noreply.github.com>
2024-10-13 19:20:57 +0300

    update

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,11 +1,14 @@
-import { Swiper, Navigation, Keyboard, axios, iziToast } from './libs';
-const refs = {
+import { Swiper } from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import '../../node_modules/swiper/swiper-bundle.min.css';
+import axios from 'axios';
+document.addEventListener('DOMContentLoaded', renderReviews);
+export const refs = {
 swiper: document.querySelector('.reviews-container'),
 swiperNext: document.querySelector('.review-button-next'),
 swiperPrev: document.querySelector('.review-button-prev'),
 reviewsList: document.querySelector('.reviews-list'),
 };
-document.addEventListener('DOMContentLoaded', renderReviews);
 const swiperForReviews = new Swiper(refs.swiper, {
 grabCursor: true,
 direction: 'horizontal',
@@ -28,7 +31,7 @@ breakpoints: {
     },
     },
 },
-modules: [Navigation, Keyboard],
+modules: [Navigation, Pagination],
 navigation: {
     nextEl: refs.swiperNext,
     prevEl: refs.swiperPrev,
@@ -44,45 +47,25 @@ const response = (
 return response;
 }
 async function renderReviews() {
+
     try {
-        const reviewsData = await getReviews();
+    const reviewsData = await getReviews();
         const markup = reviewsData
-            .map(({ author, last_name, avatar_url, review }) => {
-                if (author === "Natalia" && last_name === "Shevchenko") {
-                    return `
-                        <li class="reviews-list-item swiper-slide">
-                            <p class="reviews-text">Work with was extraordinary! He turned out to be a very competent and responsible specialist. The projects were
-                                completed on time and the result exceeded my expectations</p>
+            .map(({ author, avatar_url, review }) => {
+                return `<li class="reviews-list-item swiper-slide">
+                            <p class="reviews-text">${review}</p>
                             <div class="reviewer-info">
-                                <img class="reviewers-avatar" src="/mnt/data/2d39cdc7457392ed8725fff74b7f7ab6.png" alt="reviewer avatar" loading="lazy" />
-                                <h3 class="reviewers-name">Natalia Shevchenko</h3>
+                                <img class="reviewers-avatar" src="${avatar_url}" alt="reviewer avatar" loading="lazy" />
+                                <h3 class="reviewers-name">${author}</h3>
                             </div>
                         </li>`;
-                } else if (author === "Dmytro" && last_name === "Nazarenko") {
-                    return `
-                        <li class="reviews-list-item swiper-slide">
-                            <p class="reviews-text"> I have the honor to recommend him as an exceptional professional in his field. His knowledge and expertise are
-                                undeniable. Cooperation with him always brings impressive results.</p>
-                            <div class="reviewer-info">
-                                <img class="reviewers-avatar" src="/mnt/data/5e6640d08d65d54b4b69830550340abc.png" alt="reviewer avatar" loading="lazy" />
-                                <h3 class="reviewers-name">Dmytro Nazarenko</h3>
-                            </div>
-                        </li>`;
-                } 
             })
             .join("");
-
         refs.reviewsList.insertAdjacentHTML('beforeend', markup);
     } catch (error) {
         refs.reviewsList.insertAdjacentHTML(
             'beforeend',
             '<li class="error-mock"><p>SORRY, NOTHING TO SHOW HERE</p></li>'
         );
-        iziToast.info({
-            message: 'We`re sorry, but reviews list is currently unavailable',
-            position: 'bottomRight',
-            timeout: 2000,
-            icon: '',
-        });
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import { form, emailInput, errorMessage } from './js/work-together';
 // import { form } from './js/work-together';
 import { themeToggle } from './js/toogle';
+import { refs } from "./js/reviews";
