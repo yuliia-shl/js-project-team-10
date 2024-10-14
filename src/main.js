8575bec6ac977de1716464074dc474fc6572be69
Yuli <yu.expert@gmail.com>
2024-10-14 23:04:48 +0600

    comment toggle at main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { form, emailInput, errorMessage } from './js/work-together.js';
-import { themeToggle } from './js/toogle.js';
+// import { themeToggle } from './js/toogle.js';
 import { refs } from './js/reviews';
 
 import { initHeader } from './js/menu.js';
