32c1a6fbdbb375166bcfb41b16f9c945489db4c6
sashko-kapillar <kapillarua@gmail.com>
2024-10-14 19:01:11 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { form, emailInput, errorMessage } from './js/work-together.js';
 import { themeToggle } from './js/toogle.js';
-import { form, emailInput, errorMessage } from './js/work-together';
 import { refs } from './js/reviews';
 
 import { initHeader } from './js/menu.js';
