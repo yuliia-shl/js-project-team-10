4a5431fdd1b895d0cda1f1daac2f8499dac463f6
Mishagul <169081080+Mishagul@users.noreply.github.com>
2024-10-14 13:09:54 +0300

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
 import { form, emailInput, errorMessage } from './js/work-together';
 // import { form } from './js/work-together';
-<<<<<<< HEAD
-import { themeToggle } from './js/toogle';
-import { refs } from "./js/reviews";
-=======
 // import { themeToggle } from './js/toogle';
-
-import { initHeader } from './js/menu.js';
-initHeader();
->>>>>>> main
+import { refs } from "./js/reviews";
